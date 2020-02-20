2faae910614fd890d4b69c11a0d967863761afca
Laura <laura.heddleston@gmail.com>
2020-02-20 15:51:43 -0800

    Update App to use containers

--- src/components/App.js
+++ src/components/App.js
@@ -40,25 +40,20 @@ class App extends React.Component {
     );
   }
 
-  //TODO: swap out the React components below for the container components
-  //  you wrote in the 'containers' directory.
   render() {
     return (
       <div>
         <nav className="navbar">
           <div className="col-md-6 col-md-offset-3">
-            <Search getYouTubeVideos={this.getYouTubeVideos} />
+            <SearchContainer />
           </div>
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <VideoPlayer video={this.state.currentVideo} />
+            <VideoPlayerContainer />
           </div>
           <div className="col-md-5">
-            <VideoList
-              handleVideoListEntryTitleClick={this.handleVideoListEntryTitleClick.bind(this)}
-              videos={this.state.videos}
-            />
+            <VideoListContainer />
           </div>
         </div>
       </div>
