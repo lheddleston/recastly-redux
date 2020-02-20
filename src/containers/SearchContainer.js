a4c3027cdac1da894f874cd86a764b79b9c9284f
Laura <laura.heddleston@gmail.com>
2020-02-20 15:40:21 -0800

    Set up App containers

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,7 +2,11 @@ import { connect } from 'react-redux';
 import Search from './../components/Search.js';
 import handleSearchChange from '../actions/search.js';
 
-var SearchContainer = () => {};
+const mapDispatchToProps = {
+  handleSearchInputChange: handleVideoSearch
+}
+
+const SearchContainer = connect(null, mapDispatchToProps)(Search);
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,9 +2,19 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideo from './../actions/currentVideo.js';
 
-var VideoListContainer = () => {};
+const mapDispatchToProps = (state) => {
+  return {
+    videos: state.VideoList
+  };
+}
+const mapDispatchToProps = {
+  handleVideoListEntryTitleClick: changeVideo
+}
+
+const VideoListContainer = connect(mapStateToProps, mapDispatchToProps)(VideoList);
 
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
 
 export default VideoListContainer;
+
